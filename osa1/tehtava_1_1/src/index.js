63ab186f37d1ea75a0df93e289b45ea5d1f59ea3
Reetu Kontio <reetu.kontio@aalto.fi>
2018-09-30 18:40:01 +0300

    osa1 tasks 1.1-1.5

---
+++ osa1/tehtava_1_1/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+    return (
+        <div>
+            <h1>{props.kurssi}</h1>
+        </div>
+    )
+}
+const Sisalto = (props) => {
+    return (
+        <div>
+            <Osa osa={props.osat[0]} />
+            <Osa osa={props.osat[1]} />
+            <Osa osa={props.osat[2]} />
+        </div>
+    )
+}
+const Osa = (props) => {
+    return (
+        <div>
+            <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+        </div>
+    )
+}
+const Yhteensa = (props) => {
+    return (
+        <div>
+            <p>yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia} tehtävää</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+          {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10
+          },
+          {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+          },
+          {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+          }
+        ]
+      }
+
+    return (
+        <div>
+            <Otsikko kurssi={kurssi.nimi} />
+            <Sisalto osat={kurssi.osat} />
+            <Yhteensa osat={kurssi.osat} />
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
