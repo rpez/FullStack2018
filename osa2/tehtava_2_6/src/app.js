ab317b3cf728a52c9d19e4dae051e71a4143de6a
Reetu Kontio <reetu.kontio@aalto.fi>
2018-11-04 14:54:00 +0200

    2.8

--- osa2/tehtava_2_6/src/app.js
+++ osa2/tehtava_2_6/src/app.js
@@ -5,25 +5,33 @@ class App extends React.Component {
         super(props)
         this.state = {
             persons: [
-                { name: 'Arto Hellas' }
+                {
+                    name: 'Arto Hellas',
+                    number: '040-123456'
+                }
             ],
-            newName: ''
+            newName: '',
+            newNumber: ''
         }
     }
     handleNameChange = (event) => {
-        console.log(event.target.value)
         this.setState({ newName: event.target.value })
+    }
+    handleNumberChange = (event) => {
+        this.setState({ newNumber: event.target.value })
     }
     addName = (event) => {
         event.preventDefault()
         const nameObject = {
-            name: this.state.newName
+            name: this.state.newName,
+            number: this.state.newNumber
         }
         if (!this.state.persons.map(person => person.name).includes(nameObject.name)) {
             const persons = this.state.persons.concat(nameObject)
             this.setState({
                 persons: persons,
-                newName: ''
+                newName: '',
+                newNumber: ''
             })
         }
     }
@@ -39,11 +47,26 @@ class App extends React.Component {
                         />
                     </div>
                     <div>
+                        numero: <input
+                            value={this.state.newNumber}
+                            onChange={this.handleNumberChange}
+                        />
+                    </div>
+                    <div>
                         <button type="submit">lisää</button>
                     </div>
                 </form>
                 <h2>Numerot</h2>
-                {this.state.persons.map(person => <div key={person.name}>{person.name}</div>)}
+                <table>
+                    <tbody>
+                        {this.state.persons.map(person =>
+                            <tr key={person.name}>
+                                <td>{person.name}</td>
+                                <td>{person.number}</td>
+                            </tr>)}
+                    </tbody>
+                </table>
+
             </div>
         )
     }
