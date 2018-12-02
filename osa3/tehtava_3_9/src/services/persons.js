5e4f70058007e77504f8bbc58c30642677885362
Reetu Kontio <reetu.kontio@aalto.fi>
2018-12-02 17:53:17 +0200

    3.10

--- osa3/tehtava_3_9/src/services/persons.js
+++ osa3/tehtava_3_9/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://thawing-lake-52892.herokuapp.com/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
