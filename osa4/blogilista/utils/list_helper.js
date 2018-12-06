eda92f3eaabac1913ceaff9cb0c8825dfebb8ba0
Reetu Kontio <reetu.kontio@aalto.fi>
2018-12-07 00:37:12 +0200

    4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+
+    return blogs.map(x => x.likes).reduce(reducer, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
