e5a636b994f1c436596ca052ddefd5519cef95dc
Reetu Kontio <reetu.kontio@aalto.fi>
2018-12-08 03:51:41 +0200

    tokens

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -8,39 +9,6 @@ blogsRouter.get('/', async (request, response) => {
             .find({})
             .populate('user', { username: 1, name: 1 })
         response.json(blogs.map(Blog.format))
-    } catch (exeption) {
-        console.log(exeption)
-        response.status(500).json({ error: 'something went wrong...' })
-    }
-})
-
-blogsRouter.post('/', async (request, response) => {
-    try {
-        const body = new Blog(request.body)
-
-        if (body.url === undefined || body.title === undefined) {
-            return response.status(400).json({ error: 'title or url missing' })
-        }
-
-        const user = await User.findById(body.user)
-
-        newLikes = 0
-        if (body.likes !== undefined) newLikes = body.likes
-
-        const blog = new Blog({
-            title: body.title,
-            author: body.author,
-            url: body.url,
-            likes: newLikes,
-            user: user._id
-        })
-
-        const savedBlog = await blog.save()
-
-        user.blogs = user.blogs.concat(savedBlog._id)
-        await user.save()
-
-        response.status(201).json(Blog.format(savedBlog))
     } catch (exeption) {
         console.log(exeption)
         response.status(500).json({ error: 'something went wrong...' })
@@ -73,6 +41,58 @@ blogsRouter.put('/:id', async (request, response) => {
     } catch (exeption) {
         console.log(exeption)
         response.status(400).json({ error: 'malformatted id' })
+    }
+})
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    try {
+        const token = getTokenFrom(request)
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+
+        if (body.url === undefined || body.title === undefined) {
+            return response.status(400).json({ error: 'title or url missing' })
+        }
+
+        newLikes = 0
+        if (body.likes !== undefined) newLikes = body.likes
+
+        const user = await User.findById(decodedToken.id)
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: newLikes,
+            user: user._id
+        })
+
+        const savedBlog = await blog.save()
+
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+
+        response.status(201).json(Blog.format(savedBlog))
+    } catch (exception) {
+        if (exception.name === 'JsonWebTokenError') {
+            response.status(401).json({ error: exception.message })
+        } else {
+            console.log(exception)
+            response.status(500).json({ error: 'something went wrong...' })
+        }
     }
 })
 
