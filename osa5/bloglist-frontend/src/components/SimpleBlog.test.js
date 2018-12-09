823892223299c55048ce9174e43ab56e3884ca92
Reetu Kontio <reetu.kontio@aalto.fi>
2018-12-09 02:01:34 +0200

    5.13

--- osa5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ osa5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
-describe.only('<Note />', () => {
+describe.only('<SimpleBlog />', () => {
     const blog = {
         title: 'Title',
         author: 'Author',
@@ -22,5 +22,22 @@ describe.only('<Note />', () => {
         const titleAuthorDiv = blogComponent.find('.likes')
 
         expect(titleAuthorDiv.text()).toContain(blog.likes)
+    })
+
+    it('clicking the button twice event handler twice', () => {
+        const mockHandler = jest.fn()
+
+        const blogComponent = shallow(
+            <SimpleBlog
+                blog={blog}
+                onClick={mockHandler}
+            />
+        )
+
+        const button = blogComponent.find('button')
+        button.simulate('click')
+        button.simulate('click')
+
+        expect(mockHandler.mock.calls.length).toBe(2)
     })
 })
