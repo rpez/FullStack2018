93df6be73438e0f1f0697bdb4f6dd16c437d1dc8
Reetu Kontio <reetu.kontio@aalto.fi>
2018-12-14 15:56:55 +0200

    5.20

--- osa5/redux-anecdotes/src/App.js
+++ osa5/redux-anecdotes/src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Anecdotes</h2>
-        {anecdotes.map(anecdote =>
+        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
           <div key={anecdote.id}>
             <div>
               {anecdote.content}
